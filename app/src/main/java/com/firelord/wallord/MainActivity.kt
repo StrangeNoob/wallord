eb0965dc4dbe8833b63f23091bb1079f0cea2f25
FireLord1 <firelord.xda@lineageos.org>
2018-07-16 11:58:49 +0530

    wallord: Add colors in bottomNav
    Signed-off-by: FireLord1 <firelord.xda@lineageos.org>

--- app/src/main/java/com/firelord/wallord/MainActivity.kt
+++ app/src/main/java/com/firelord/wallord/MainActivity.kt
@@ -8,6 +8,8 @@ import com.firelord.wallord.fragments.AllPhotosFragment
 import com.firelord.wallord.fragments.HomeFragment
 import com.firelord.wallord.fragments.SupportFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import android.graphics.Color
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,11 +20,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val tabColors = applicationContext.resources.getIntArray(R.array.tab_colors)
         navigationAdapter = AHBottomNavigationAdapter(this,R.menu.navigation)
-        navigationAdapter.setupWithBottomNavigation(bottom_navigation)
+        navigationAdapter.setupWithBottomNavigation(bottom_navigation, tabColors)
+
+        // Change colors
+        bottom_navigation.setDefaultBackgroundColor(Color.parseColor("#FEFEFE"));
+        bottom_navigation.setAccentColor(Color.parseColor("#F63D2B"));
+        bottom_navigation.setInactiveColor(Color.parseColor("#747474"));
+
+        // Force to tint the drawable (useful for font with icon for example)
+        bottom_navigation.setForceTint(true);
+
+        // Display color under navigation bar (API 21+)
+        bottom_navigation.setTranslucentNavigationEnabled(true);
+
+        // Use colored navigation with circle reveal effect
+        bottom_navigation.setColored(true);
 
         bottom_navigation.setOnTabSelectedListener({ position, wasSelected ->
-
             when(position){
                 0 -> fragment = HomeFragment()
                 1 -> fragment = AllPhotosFragment()
